90de445fc499facd4481b6303ba5f9104bc804e0
abidahmed <md.abid123@gmail.com>
2013-05-06 17:20:11 +0530

    sample files

---
+++ src/uk/co/wowcher/cashback/persistence/dao/Sample.java
@@ -0,0 +1,13 @@
+package uk.co.wowcher.cashback.persistence.dao;
+
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
